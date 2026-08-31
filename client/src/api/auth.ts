a287eaf2aa283fcbd5189195dfb9e51fc1bfc7af
Ageyan <ageenko01114444@gmail.com>
2026-08-31 12:30:16 +0300

    Fix auth.ts

--- client/src/api/auth.ts
+++ client/src/api/auth.ts
@@ -2,11 +2,11 @@ import api from "./api";
 import type { LoginResponse, RegisterResponse } from "../types/auth.types";
 
 export const login = async (userName: string, password: string): Promise<LoginResponse> => {
-    const { data } = await api.post<LoginResponse>('/auth/login', { userName, password });
+    const { data } = await api.post<LoginResponse>('/api/auth/login', { userName, password });
     return data;
 }
 
 export const register = async (userName: string, password: string): Promise<RegisterResponse> => {
-    const { data } = await api.post<RegisterResponse>('/auth/register', { userName, password });
+    const { data } = await api.post<RegisterResponse>('/api/auth/register', { userName, password });
     return data;
 }
